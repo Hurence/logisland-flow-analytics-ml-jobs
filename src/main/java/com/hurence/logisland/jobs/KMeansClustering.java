ac3ae9e8055a758a42f1d1c51648df00df96921d
tkarra <tariq.karra@gmail.com>
2017-06-28 17:45:48 +0200

    * Add binetflow date conversion * Add splitText processor properties * Add more information in flows needed for trace computation * Display cluster centers in console

--- src/main/java/com/hurence/logisland/jobs/KMeansClustering.java
+++ src/main/java/com/hurence/logisland/jobs/KMeansClustering.java
@@ -7,6 +7,7 @@ import com.hurence.logisland.botsearch.Trace;
 import com.hurence.logisland.processor.Processor;
 import com.hurence.logisland.processor.SplitText;
 import com.hurence.logisland.processor.StandardProcessContext;
+import com.hurence.logisland.processor.UpdateBiNetflowDate;
 import com.hurence.logisland.record.Record;
 import com.hurence.logisland.record.RecordUtils;
 import org.apache.spark.SparkConf;
@@ -41,13 +42,21 @@ public class KMeansClustering {
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
         // Read data file from Hadoop file system.
-        String path = "hdfs://sandbox.hortonworks.com:8020/user/hurence/flows.txt";
+        //String path = "hdfs://sandbox.hortonworks.com:8020/user/hurence/flows.txt";
+        String path = "file:///D:\\perso\\Developpement\\logisland-flow-analytics-ml-jobs\\resources\\light_capture_100000.txt";
 
         // Read the data file and return it as RDD of strings
         JavaRDD<String> linesRDD = sc.textFile(path);
 
+        // Split Text Processor :
         Processor splitTextProcessor = new SplitText();
-        StandardProcessContext context = new StandardProcessContext(splitTextProcessor, "dummy");
+        StandardProcessContext splitTextContext = new StandardProcessContext(splitTextProcessor, "splitTextProcessor");
+        splitTextContext.setProperty("value.fields", "timestamp,duration,protocol,src_ip,src_port,direction,dest_ip,dest_port,state,src_tos,dest_tos,packets_out,bytes_out,bytes_in,label");
+        splitTextContext.setProperty("value.regex", "(\\d{4}\\/\\d{2}\\/\\d{2}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}\\.\\d{0,6}),([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,([^,]+)?,flow=([^,]+)");
+
+        // BiNetFlow Processor :
+        Processor updateBiNetflowDate = new UpdateBiNetflowDate();
+        StandardProcessContext updateBiNetflowDateContext = new StandardProcessContext(updateBiNetflowDate, "updateBiNetflowDate");
 
         PairFunction<String, String, Record> mapFunction = new PairFunction<String, String, Record>() {
             public Tuple2<String, Record> call(String line) {
@@ -55,10 +64,12 @@ public class KMeansClustering {
                 Record r = RecordUtils.getKeyValueRecord("", line);
                 List list = new ArrayList<Record>();
                 list.add(r);
-                Collection<Record> records = splitTextProcessor.process(context, list);
+                Collection<Record> tempRecords = splitTextProcessor.process(splitTextContext, list);
+                Collection<Record> records = updateBiNetflowDate.process(updateBiNetflowDateContext, tempRecords);
+
                 Record record = records.iterator().next();
-                String ipSource = record.getField("ip_source").asString();
-                String ipTarget = record.getField("ip_target").asString();
+                String ipSource = record.getField("src_ip").asString();
+                String ipTarget = record.getField("dest_ip").asString();
 
                 return new Tuple2<>(ipSource + "_" + ipTarget, record);
             }
@@ -82,11 +93,13 @@ public class KMeansClustering {
 
                         // set up the flows buffer
                         ArrayList<HttpFlow> flows = new ArrayList<HttpFlow>();
-                        flowRecords.forEach(f -> {
+                        flowRecords.forEach(flowRecord -> {
                             HttpFlow flow = new HttpFlow();
-                            flow.setDate(new java.util.Date(f.getField("timestamp").asLong()));
-                            flow.setRequestSize(f.getField("requestSize").asLong());
-                            flow.setResponseSize(f.getField("responseSize").asLong());
+                            flow.setDate(new java.util.Date(flowRecord.getField("record_time").asLong()));
+                            flow.setipSource(flowRecord.getField("src_ip").asString());
+                            flow.setIpTarget(flowRecord.getField("dest_ip").asString());
+                            flow.setRequestSize(flowRecord.getField("bytes_in").asLong());
+                            flow.setResponseSize(flowRecord.getField("bytes_out").asLong());
                             flows.add(flow);
                         });
 
@@ -136,7 +149,6 @@ public class KMeansClustering {
             return new Tuple2<>(t._1, Vectors.dense(values));
         }).cache();
 
-
         // Scale the trace to get mean = 0 and std = 1
         StandardScaler scaler = new StandardScaler(true, true);
 
@@ -145,7 +157,6 @@ public class KMeansClustering {
         StandardScalerModel scalerModel = scaler.fit(tracesVector);
 
         JavaRDD<Tuple2<String, Vector>> scaledTraces = tracesTuple.map(x -> new Tuple2<>(x._1, scalerModel.transform(x._2)));
-
 
         // TODO add an automated job which compute best parameters
         // Cluster the data into two classes using KMeans
@@ -159,6 +170,19 @@ public class KMeansClustering {
 
         // Assign traces to clusters
         JavaRDD<Tuple2<String, Integer>> centroids = scaledTraces.map(t -> new Tuple2<>(t._1, clusters.predict(t._2)));
+
+        // Display cluster centers
+        Vector[] clusterCenters = clusters.clusterCenters();
+        for (int i = 0; i < clusterCenters.length; i++) {
+            Vector clusterCenter = clusterCenters[i];
+            double[] centerPoint = clusterCenter.toArray();
+            System.out.println("Cluster Center " + i + ": [ " +
+                      "'Average uploaded bytes': " + centerPoint[0] +
+                    ", 'Average downloaded bytes': " + centerPoint[1] +
+                    ", 'Average time between two flows': " + centerPoint[2] +
+                    ", 'Most Significant Frequency': " + centerPoint[3] +
+                    " ]");
+        }
 
         // TODO : transform into dataframe : .toDF("id", "centroid")
         // TODO : save into file and / or display in console
